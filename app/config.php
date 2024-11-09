c83721b19d46b73ed7b67f3a159f893e4a9ca942
RubenJunco <ruju_20@alu.uabcs.mx>
2024-11-09 16:12:12 -0700

    Login SemiTerminado

--- app/config.php
+++ app/config.php
@@ -1,5 +1,5 @@
 <?php 
 
-	define('BASE_PATH', 'http://localhost/boots/Examen/ExamenUnidadlV/');
+	define('BASE_PATH', 'http://localhost/');
 
 ?>
--- index.php
+++ index.php
@@ -25,11 +25,11 @@
       <div class="bg-overlay bg-dark"></div>
       <div class="auth-wrapper">
         <div class="auth-sidecontent"> <div class="auth-sidefooter">
-  <img src="assets/images/logo-dark.svg" class="img-brand img-fluid" alt="images" />
+  <!-- <img src="assets/images/logo-dark.png" class="img-brand img-fluid" alt="images" /> -->
   <hr class="mb-3 mt-4" />
   <div class="row">
     <div class="col my-1">
-      <p class="m-0">Made with ♥ by Team <a href="https://themeforest.net/user/phoenixcoded" target="_blank"> Phoenixcoded</a></p>
+      <p class="m-0">Made with ♥ by UABCS and Team <a href="https://themeforest.net/user/phoenixcoded" target="_blank"> Phoenixcoded</a></p>
     </div>
     <div class="col-auto my-1">
       <ul class="list-inline footer-link mb-0">
@@ -43,37 +43,39 @@
  </div>
         <div class="auth-form">
         <form action="app/AuthController.php" method="POST" class="auth-form">
-  <div class="card my-5 mx-3">
+    <div class="card p-3 mb-2 bg-black text-white" style="border: 1px solid white;">
     <div class="card-body">
-      <h4 class="f-w-500 mb-1">Login with your email</h4>
-      <p class="mb-3">Don't have an Account? <a href="register-v2.html" class="link-primary ms-1">Create Account</a></p>
+
+      <div class="text-center mb-3">
+        <img src="LogoMain.PNG" alt="Login Icon" class="img-fluid" style="max-width: 100px;">
+      </div>
+        <h4 class="f-w-500 mb-1 text-white">Login Usuario</h4>
+      <!-- <p class="mb-3">Don't have an Account? <a href="register-v2.html" class="link-primary ms-1">Create Account</a></p>  -->
       
-      <div class="mb-3">
+      <div class="p-3 mb-2 bg-body text-body rounded">
         <input type="email" class="form-control" id="floatingInput" name="email" placeholder="Email Address" required />
       </div>
       
-      <div class="mb-3">
+      <div class="p-3 mb-2 bg-body text-body rounded">
         <input type="password" class="form-control" id="floatingInput1" name="password" placeholder="Password" required />
       </div>
       
       <div class="d-flex mt-1 justify-content-between align-items-center">
         <div class="form-check">
-          <input class="form-check-input input-primary" type="checkbox" id="customCheckc1" name="remember_me" checked />
-          <label class="form-check-label text-muted" for="customCheckc1">Remember me?</label>
+         <!-- <input class="form-check-input input-primary" type="checkbox" id="customCheckc1" name="remember_me" checked /> -->
+          <!-- <label class="form-check-label text-muted" for="customCheckc1">Remember me?</label> -->
         </div>
-        <a href="forgot-password-v2.html">
-          <h6 class="text-secondary f-w-400 mb-0">Forgot Password??</h6>
-        </a>
+        
       </div>
       
-      <div class="d-grid mt-4">
-        <button type="submit" class="btn btn-primary">Login</button>
+      <div class="d-grid mt-4 ">
+        <button type="submit" class="btn btn-secondary">Login</button>
         <input type="hidden" name="action" value="login">
       </div>
       
-      <div class="saprator my-3">
+      <!--<div class="saprator my-3">
         <span>Or continue with</span>
-      </div>
+      </div> 
       
       <div class="text-center">
         <ul class="list-inline mx-auto mt-3 mb-0">
@@ -93,7 +95,7 @@
             </a>
           </li>
         </ul>
-      </div>
+      </div>-->
       
     </div>
   </div>
