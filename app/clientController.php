ff9f05238cbb34f6ea5aaf71b41f6321485c8170
Carlos Carballo <159819105+Metaler127@users.noreply.github.com>
2024-11-13 14:32:57 -0700

    Controlador de clientes

---
+++ app/clientController.php
@@ -0,0 +1,164 @@
+<?php 
+	session_start();
+
+	if (isset($_POST['action'])) {
+		switch ($_POST['action']) {
+			
+			case 'new_client':
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $phone_number = $_POST['phone_number'];
+                $is_suscribed = $_POST['is_suscribed'];
+                $level_id = $_POST['level_id'];
+                $clientController = new clientController();
+                $clientController->newClient($name, $email, $password, $phone_number, $is_suscribed, $level_id);
+			break;
+
+            case 'get_client':
+                $id = $_POST['id'];
+                $clientController = new clientController();
+                $clientController->getClient($id);
+            break;
+			
+            case 'update_client':
+                $id = $_POST['id'];
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $phone_number = $_POST['phone_number'];
+                $is_suscribed = $_POST['is_suscribed'];
+                $level_id = $_POST['level_id'];
+                $clientController = new clientController();
+                $clientController->updateUser($id, $name, $email, $password, $phone_number, $is_suscribed, $level_id);
+            break;
+
+            case 'delete_client':
+                $id = $_POST['id'];
+                $clientController = new clientController();
+                $clientController->deleteUser($id);
+            break;
+		}
+	}
+
+	class clientController {
+
+        public function getAll() {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer' . $_SESSION['user_data']->token
+            ),
+            ));   
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response; 
+        }
+
+        public function getClient($id) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients'. $id, 
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer' . $_SESSION['user_data']->token
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function newClient($name, $email, $password, $phone_number, $is_suscribed, $level_id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'name' => $name,
+                'email' => $email,
+                'password' => $password,
+                'phone_number' => $phone_number,
+                'is_suscribed' => $is_suscribed,
+                'level_id' => $level_id
+            ),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer' . $_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function updateUser($id, $name, $email, $password, $phone_number, $is_suscribed, $level_id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => 
+                'name='. $name .
+                '&email='. $email . 
+                '&password='. $password .
+                '&phone_number='. $phone_number . 
+                '&is_suscribed='. $is_suscribed .
+                '&level_id='. $level_id . 
+                '&id='. $id,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: Bearer' . $_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function deleteUser($id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/'. $id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer' . $_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+
+        }
+	}
+?>
