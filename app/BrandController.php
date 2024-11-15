ad0b56395c4e39a8c218f4f1c5862bb9492e956a
Carlos Carballo <159819105+Metaler127@users.noreply.github.com>
2024-11-14 20:27:29 -0700

    BrandController/TagsController

---
+++ app/BrandController.php
@@ -0,0 +1,153 @@
+<?php 
+	session_start();
+
+	if (isset($_POST['action'])) {
+		
+		switch ($_POST['action']) {
+			
+			case 'new_brand':
+                $name = $_POST['name'];
+                $description = $_POST['description'];
+                $slug = $_POST['slug'];
+                $brandController = new BrandController();
+                $brandController->newBrand($name, $description, $slug);
+            break;
+
+            case 'update_brand':
+                $name = $_POST['name'];
+                $description = $_POST['description'];
+                $slug = $_POST['slug'];
+                $id = $_POST['id'];
+                $brandController = new BrandController();
+                $brandController->newBrand($name, $description, $slug, $id);
+            break;
+
+            case 'delete_brand':
+                $id = $_POST['id'];
+                $brandController = new BrandController();
+                $brandController->deleteBrand($id);
+            break;
+		}
+	}
+
+	class BrandController {
+        public function newBrand($name, $description, $slug) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'name' => $name,
+                'description' => $description,
+                'slug' => $slug
+            ),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function getAll(){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);  
+            $response = json_decode($response);
+
+            if (isset($response->code) && $response->code > 0) {
+                return $response->data;
+            }else{
+                return [];
+            }
+        }
+
+        public function getBrand($id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands/'. $id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function updateBrand($name, $description, $slug, $id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => 
+                'name='.$name.
+                '&description='.$description.
+                '&slug='.$slug.
+                '&id='.$id,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: Bearer '.$_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+
+        public function deleteBrand($id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands/'.$id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['user_data']->token
+            ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            echo $response;
+        }
+	}
+?>
+
+
